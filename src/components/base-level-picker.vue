6993c6a7d4c8863a64ed41ef57208bc68f9315be
kernel <karasinskikornel@gmail.com>
2023-05-21 17:42:00 +0200

    add header and base-level-picker

---
+++ src/components/base-level-picker.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="picker">
+        <div class="picker__title">ZACHMURZENIE</div>
+        <div class="picker__options">Wybierz...</div>
+    </div>
+</template>
+
+<style lang="scss">
+.picker {
+    
+
+    &__title {
+        background-color: #272747;
+        color: #EBED88;;
+        font-size: 32px;
+    }
+
+    &__options {
+        background-color: #B8B8B8;
+        font-size: 28px;
+    }
+}
+</style>
+
+<script>
+
+
+</script>
