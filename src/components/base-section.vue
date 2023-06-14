22d0d27285d462f53e6581acd5ffcfae5b5c3f1e
Łukasz Matuszczak <35471793+mlodamrowa@users.noreply.github.com>
2023-06-14 19:18:52 +0200

    Moon Phase Update

--- src/components/base-section.vue
+++ src/components/base-section.vue
@@ -11,7 +11,8 @@
             </div>
         </div>
         <div class="section__main">
-            <div class="section__main-photo">Jakiś JPG</div>
+            <div class="section__main-photo"><img :src="MoonPhaseOptions[selectedOptionMoonPhase].img"
+                                                  :alt="MoonPhaseOptions[selectedOptionMoonPhase].alt"></div>
             <div class="section__main-article">
                 <div class="section__main-article-title"> {{ MoonPhaseOptions[selectedOptionMoonPhase].label }}</div>
                 <div class="section__main-article-paragraph">{{
@@ -56,9 +57,13 @@
     margin-top: 32px;
 
     &-photo {
-      padding: 100px;
       background-color: beige;
       margin-left: 40px;
+
+      img {
+        height: 200px;
+          width: 200px;
+      }
     }
 
     &-article {
@@ -85,48 +90,69 @@ export default {
         return {
             selectedOptionMoonPhase: 1,
             MoonPhaseOptions: [
-                {id: 0, value: "0", label: "Wybierz", paragraph: "Wybierz fazę księżyca"},
+                {
+                    id: 0,
+                    value: "0",
+                    label: "Wybierz",
+                    paragraph: "Wybierz fazę księżyca",
+                    img: '',
+                    alt: 'Wybierz zdjecie',
+                },
                 {
                     id: 1,
                     value: "1",
                     label: "Nów",
-                    paragraph: "Nów - Kiedy Księżyc jest między Ziemią a Słońcem, z naszej perspektywy nie widzimy oświetlonej części Księżyca. Jest to początek nowego cyklu."
+                    paragraph: "Nów - Kiedy Księżyc jest między Ziemią a Słońcem, z naszej perspektywy nie widzimy oświetlonej części Księżyca. Jest to początek nowego cyklu.",
+                    img: 'img/Crescent_Moon.jpg',
+                    alt: 'Nów',
                 },
                 {
                     id: 2,
                     value: "2",
                     label: "Sierp",
-                    paragraph: "Sierp - Kiedy Księżyc jest w fazie sierpa, widzimy tylko niewielką część oświetlonej strony, tworzącą kształt sierpa."
+                    paragraph: "Sierp - Kiedy Księżyc jest w fazie sierpa, widzimy tylko niewielką część oświetlonej strony, tworzącą kształt sierpa.",
+                    img: 'img/Sierp.jpg',
+                    alt: 'Sierp',
                 },
                 {
                     id: 3,
                     value: "3",
                     label: "I kwadra",
-                    paragraph: "I kwadra - W tej fazie widzimy połowę Księżyca, która jest oświetlona. Jest to połowa, która jest widoczna południowo."
+                    paragraph: "I kwadra - W tej fazie widzimy połowę Księżyca, która jest oświetlona. Jest to połowa, która jest widoczna południowo.",
+                    img: 'img/1280px-Logroño_moon_waxing_half_moon.jpg',
+                    alt: 'I kwadra'
                 },
                 {
                     id: 4,
                     value: "4",
                     label: "Pełnia",
-                    paragraph: "Gibbous (pełnia) - Kiedy Księżyc jest w pełni oświetlony, mamy fazę pełni. Widzimy całą oświetloną stronę Księżyca."
+                    paragraph: "Gibbous (pełnia) - Kiedy Księżyc jest w pełni oświetlony, mamy fazę pełni. Widzimy całą oświetloną stronę Księżyca.",
+                    img: 'img/FullMoon.jpg',
+                    alt: 'Pełnia'
                 },
                 {
                     id: 5,
                     value: "5",
                     label: "III kwadra",
-                    paragraph: "III kwadra - W tej fazie widzimy połowę Księżyca, która jest oświetlona. Jest to połowa, która jest widoczna północno."
+                    paragraph: "III kwadra - W tej fazie widzimy połowę Księżyca, która jest oświetlona. Jest to połowa, która jest widoczna północno.",
+                    img: 'img/HalfMoon.jpg',
+                    alt: 'III kwadra'
                 },
                 {
                     id: 6,
                     value: "6",
                     label: "Sierp III",
-                    paragraph: "Sierp - Po III kwadrze Księżyc przechodzi znowu w fazę sierpa, tym razem w odwrotnej orientacji niż na początku cyklu."
+                    paragraph: "Sierp - Po III kwadrze Księżyc przechodzi znowu w fazę sierpa, tym razem w odwrotnej orientacji niż na początku cyklu.",
+                    img: 'img/Sierp.jpg',
+                    alt: 'Sierp III'
                 },
                 {
                     id: 7,
                     value: "7",
                     label: "Powrót do nów",
-                    paragraph: "Powrót do nów - Cykl kończy się powrotem do fazy nów, a następnie zaczyna się od nowa."
+                    paragraph: "Powrót do nów - Cykl kończy się powrotem do fazy nów, a następnie zaczyna się od nowa.",
+                    img: 'img/Crescent_Moon.jpg',
+                    alt: 'Powrót do nów'
                 },
             ],
 
